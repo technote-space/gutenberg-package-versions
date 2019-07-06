4b00d2bdd6a6573913ccfa1e95a756e138fabf7e
Technote <technote.space@gmail.com>
2019-07-07 01:28:53 +0900

    fix

--- bin/deploy/env.sh
+++ bin/deploy/env.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-current=$(cd $(dirname $0);
-pwd)
-source ${current}/../variables.sh
+if [[ -z "${TRAVIS_BUILD_DIR}" ]]; then
+	echo "<TRAVIS_BUILD_DIR> is required"
+	exit 1
+fi
 
 export RELEASE_FILE1=data.zip
-export RELEASE_FILE2=${VERSION_FILE}
+export RELEASE_FILE2=versions.json
 export RELEASE_TITLE=${TRAVIS_TAG}
 export RELEASE_TAG=${TRAVIS_TAG}
 export RELEASE_BODY="Auto updated (Travis build: ${TRAVIS_BUILD_WEB_URL})"
