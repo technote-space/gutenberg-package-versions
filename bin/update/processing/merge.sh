89c24ad843fc92ceb0ae1159f56274e811999ffd
Technote <technote.space@gmail.com>
2019-07-06 23:53:12 +0900

    fix

--- bin/update/processing/merge.sh
+++ bin/update/processing/merge.sh
@@ -16,10 +16,8 @@ fi
 echo ""
 echo ">> Merge version files"
 
-PREFIX='{"'
-SUFFIX='":.[]}'
 find ${VERSION_DIR}/*.json -maxdepth 1 -type f -print0\
  | xargs --no-run-if-empty -0 -I filename basename filename .json\
- | xargs -I tag jq -s ${PREFIX}tag${SUFFIX} ${VERSION_DIR}/tag.json\
+ | xargs -I {} jq -s '{"'{}'":.[]}' ${VERSION_DIR}/{}.json\
  | jq -sc .\
  > ${TRAVIS_BUILD_DIR}/${VERSION_FILE}
