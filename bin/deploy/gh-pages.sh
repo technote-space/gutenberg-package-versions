7054fa32d6cb2a94279bfbd47a7541d805ae077d
Technote <technote.space@gmail.com>
2019-07-08 05:38:33 +0900

    trivial changes

--- bin/deploy/gh-pages.sh
+++ bin/deploy/gh-pages.sh
@@ -12,9 +12,15 @@ if [[ ! -d ${DATA_DIR} ]]; then
 fi
 
 echo ""
+echo ">> Clone gh-pages"
+git clone -b gh-pages "https://github.com/${TRAVIS_REPO_SLUG}.git" ${GH_PAGES_DIR}
+
+echo ""
 echo ">> Prepare files"
-mkdir -p ${GH_PAGES_DIR}/${GH_PAGES_API_ROOT}
-cp -a ${DATA_DIR} ${GH_PAGES_DIR}/${GH_PAGES_API_ROOT}/${GH_PAGES_API_VERSION}
+mkdir -p ${GH_PAGES_DIR}/${GH_PAGES_API_ROOT}/${GH_PAGES_API_VERSION}
+rm -f ${GH_PAGES_DIR}/README.md
+rm -f ${GH_PAGES_DIR}/CNAME
+rsync -a --checksum --delete ${DATA_DIR}/ ${GH_PAGES_DIR}/${GH_PAGES_API_ROOT}/${GH_PAGES_API_VERSION}/
 cp ${TRAVIS_BUILD_DIR}/README.md ${GH_PAGES_DIR}/
 echo ${GH_PAGES_CNAME} > ${GH_PAGES_DIR}/CNAME
 
@@ -25,11 +31,10 @@ fi
 
 echo ""
 echo ">> Commit"
-git -C ${GH_PAGES_DIR} init
 git -C ${GH_PAGES_DIR} add --all
 git -C ${GH_PAGES_DIR} status --short
 git -C ${GH_PAGES_DIR} commit -m "${COMMIT_MESSAGE}"
 
 echo ""
 echo ">> Push"
-git -C ${TRAVIS_BUILD_DIR} push --force "https://github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
+git -C ${TRAVIS_BUILD_DIR} push origin gh-pages
