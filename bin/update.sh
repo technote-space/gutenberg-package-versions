46940dfc529aeee858cba7d94a3bf4e4cfca5b9b
Technote <technote.space@gmail.com>
2019-07-13 14:53:04 +0900

    refactor: refactoring

--- bin/update.sh
+++ bin/update.sh
@@ -21,4 +21,4 @@ bash ${current}/update/processing.sh ${GUTENBERG_VARIABLE_PREFIX}&
 bash ${current}/update/processing.sh ${WP_VARIABLE_PREFIX}&
 wait
 
-bash ${TRAVIS_BUILD_DIR}/travis-ci/bin/update/commit.sh ${DATA_DIR}
+bash ${SCRIPT_DIR}/update/commit.sh ${DATA_DIR}
