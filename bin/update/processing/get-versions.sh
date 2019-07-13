35a4e090a96829a1df845613a1c98d0f478878cb
Technote <technote.space@gmail.com>
2019-07-14 02:02:29 +0900

    feat: remove private package (#33)

--- bin/update/processing/get-versions.sh
+++ bin/update/processing/get-versions.sh
@@ -14,11 +14,11 @@ source ${current}/../../variables.sh ${1}
 mkdir -p ${TARGET_PUBLISH_TAGS_DIR}
 find ${TARGET_WORK_TAG_DIR} -maxdepth 1 -type d -print0 | xargs -n1 --no-run-if-empty -0 basename | grep -v "tags" | while read -r tag;
 do
-	if [[ -f ${TARGET_WORK_TAG_DIR}/${tag}/package.json ]] && [[ -d ${TARGET_WORK_TAG_DIR}/${tag}/packages ]] && [[ ! -f ${TARGET_PUBLISH_TAGS_DIR}/${tag}.json ]]; then
+	if [[ -f ${TARGET_WORK_TAG_DIR}/${tag}/package.json ]] && [[ -d ${TARGET_WORK_TAG_DIR}/${tag}/packages ]]; then
 		echo ""
 		echo ">> ${tag}"
 		jq -sc . $(find ${TARGET_WORK_TAG_DIR}/${tag}/packages/*.json -maxdepth 1 -type f)\
-		 | jq -rc '[ .[] | {key:.name, value:.version} ] | from_entries'\
+		 | jq -rc '[ .[] | select( has( "private" ) | not) | {key:.name, value:.version} ] | from_entries'\
 		 | sed 's/@wordpress\//wp-/g'\
 		 > ${TARGET_PUBLISH_TAGS_DIR}/${tag}.json
 	fi
